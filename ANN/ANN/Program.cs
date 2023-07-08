d65a59838ef1a40b819ad6ade3c3dc0b57a21176
Midhun K S <61262745+midhunks6460@users.noreply.github.com>
2023-07-09 01:15:28 +0530

    Add files via upload

---
+++ ANN/ANN/Program.cs
@@ -0,0 +1,24 @@
+﻿using ArtificialNeuralNetwork;
+using ArtificialNeuralNetwork.Factories;
+namespace ConsoleApp1
+{
+    class Example
+    { 
+        public static void Main(string[] args)
+        {
+            int ips = 3;
+            int ops = 1;
+            int hlayer = 1;
+            int nhlayer = 5;
+
+            INeuralNetwork network = NeuralNetworkFactory.GetInstance().Create(ips, ops, hlayer, nhlayer);
+
+            double[] inputs = new double[] { 1.0, 2.5, 3.0 };
+            network.SetInputs(inputs);
+            network.Process();
+            double[] outputs = network.GetOutputs();
+
+            System.Diagnostics.Debug.WriteLine(outputs.Length);
+        }
+    }
+}
